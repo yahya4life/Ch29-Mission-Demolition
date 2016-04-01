ab7dbd2c0b484c6bdcffca8841ca257255f4ea9b
yahya <yahyamehkri@yahoo.com>
2016-03-31 20:15:34 -0400

    Mission changes

---
+++ Assets/__Scripts/Goal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+	static public bool goalMet = false;
+	// Use this for initialization
+	void OnTriggerEnter ( Collider other) {
+		if (other.gameObject.tag =="Projectile" ) {
+			Goal.goalMet = true;
+			Color c = GetComponent<Renderer>().material.color;
+			c.a = 1;
+			GetComponent<Renderer>().material.color = c;
+		}
+			}
+			}
